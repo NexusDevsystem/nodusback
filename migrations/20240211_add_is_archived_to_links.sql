0cdf110135e895a3b1bf5a0710e899c9cda15607
Nexus Devsystem <nexusdevsystem@gmail.com>
2026-02-13 03:18:37 -0300

    Atualização

--- migrations/20240211_add_is_archived_to_links.sql
+++
@@ -1,8 +0,0 @@
--- Add is_archived column to links table if it doesn't exist
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
-                   WHERE table_name='links' AND column_name='is_archived') THEN 
-        ALTER TABLE links ADD COLUMN is_archived BOOLEAN DEFAULT FALSE;
-    END IF;
-END $$;
